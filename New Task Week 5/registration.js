db00447d086ec86329e8196fa112767ced7af8a6
Maryann878 <85343201+Maryann878@users.noreply.github.com>
2021-07-14 03:35:07 +0100

    Add files via upload

---
+++ New Task Week 5/registration.js
@@ -0,0 +1,61 @@
+const firstn = document.getElementById('fname')
+const lastn = document.getElementById('lname')
+const pwd1 = document.getElementById('p1')
+const pwd2 = document.getElementById('p2')
+
+const subBtn = document.querySelector('#submitBtn');
+subBtn.addEventListener('click', function validate(event){
+    if (firstn.value===lastn.value) {
+        alert('firstname must not be same with lastname')
+        event.preventDefault()
+        
+    } 
+    if (pwd1.value !== pwd2.value) {
+        alert(`password did not match`)
+        event.preventDefault();
+    } else {event.submit();}
+    
+} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
